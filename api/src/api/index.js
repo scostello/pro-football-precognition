f239020be1aca236915db4eaec77a0fb9fea4895
Sean Costello <me@seancostello.io>
2018-05-27 16:54:37 -0500

    Moving from older generated ui to create-react-app

--- api/src/api/index.js
+++
@@ -1,7 +0,0 @@
-const registerRoutes = (app) => {
-    app.use('/players', require('./players/routes'));
-};
-
-module.exports = {
-    registerRoutes,
-};
--- web/src/routes/Dashboard/index.js
+++
@@ -1,6 +0,0 @@
-import DashboardView from './components/DashboardView';
-
-// Sync route definition
-export default {
-    component : DashboardView
-};
